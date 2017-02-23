e0ebce6802651efde7513da46a74c7e84dffc851
David Fountain <David Fountain>
2017-02-23 16:48:33 -0700

    created the Floor.java object and started the constructor and some of the getters. started the Person constructor and made some getters

--- Person.java
+++ Person.java
@@ -2,5 +2,43 @@
 public class Person
 {
 	//person object
-	//bla bla
+
+	private int stair_up;
+	private int stair_down;
+	private int time_spawned;
+	private int time_deleted;
+	
+	//constructor
+	public Person(Config config)
+	{
+		stair_up = Config.get_steps_per_floor_stairs_up();
+		stair_down = Config.get_steps_per_floor_stairs_down();
+		//time_spawned = ; //poisson distribution
+	}
+
+	
+
+	///////////GETTERS///////////////////////
+	public int get_floor_on()
+	{
+
+	}
+
+	public int get_floor_off()
+	{
+
+	}
+
+	public int get_stair_up()
+	{
+		return stair_up;
+	}
+
+	public int get_stair_down()
+	{
+		return stair_down;
+	}
+
+
+
 }
