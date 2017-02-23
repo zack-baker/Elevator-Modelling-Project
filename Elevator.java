3834384f97456532ed9c0e60b336d8f089e0e83a
Zack <zack.baker@lab.kingsu.ca>
2017-02-23 16:23:09 -0700

    Created Elevator class and constructor

---
+++ Elevator.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList
+public class Elevator{
+
+	private final int capacity = 12;
+	private int spf;
+	private int stationary_time
+	private boolean up;
+	private Floor cur_floor;
+	private ArrayList<Person> people;
+	private ArrayList<Floor> destinations;
+
+	public Elevator(Config c){
+		spf = c.get_steps_per_floor_elevator();
+		stationary_time = c.get_stationary_steps();
+		cur_floor = new Floor(1);
+		up = true;
+		people = new ArrayList();
+		destinations = new ArrayList();
+	}
+}
