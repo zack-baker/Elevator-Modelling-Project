77959939df27db3925069021452fc98df572df7d
Zack <zack.baker@lab.kingsu.ca>
2017-02-23 19:42:34 -0700

    Updated for the Timekeeper class

--- Elevator.java
+++ Elevator.java
@@ -1,14 +1,16 @@
-import java.util.ArrayList
+import java.util.ArrayList;
 public class Elevator{
 
 	private final int capacity = 12;
 	private int spf;
-	private int stationary_time
+	private int stationary_time;
 	private boolean up;
 	private Floor cur_floor;
 	private ArrayList<Person> people;
 	private ArrayList<Floor> destinations;
 	private double last_timestep_on_floor;
+
+
 	public Elevator(Config c, Floor start_floor){
 		spf = c.get_steps_per_floor_elevator();
 		stationary_time = c.get_stationary_steps();
@@ -19,10 +21,10 @@ public class Elevator{
 		last_timestep_on_floor = 0;
 	}
 
-	public void check_if_next_floor(int cur_timestep){
+	public void check_if_next_floor(){
 		//if enough time has passed, advance to the next floor
-		if(cur_timestep-last_timestep_on_floor>=spf){
-			
+		if(Timekeeper.get_timestep()-last_timestep_on_floor>=spf){
+
 		}
 	}
 }
