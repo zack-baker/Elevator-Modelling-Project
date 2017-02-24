0ce8397169c95ae42dc5fa960d13617ce7c43208
Zack <zack.baker@lab.kingsu.ca>
2017-02-23 19:14:46 -0700

    Started on the floor transition function

--- Elevator.java
+++ Elevator.java
@@ -8,7 +8,7 @@ public class Elevator{
 	private Floor cur_floor;
 	private ArrayList<Person> people;
 	private ArrayList<Floor> destinations;
-
+	private double last_timestep_on_floor;
 	public Elevator(Config c, Floor start_floor){
 		spf = c.get_steps_per_floor_elevator();
 		stationary_time = c.get_stationary_steps();
@@ -16,5 +16,13 @@ public class Elevator{
 		up = true;
 		people = new ArrayList();
 		destinations = new ArrayList();
+		last_timestep_on_floor = 0;
+	}
+
+	public void check_if_next_floor(int cur_timestep){
+		//if enough time has passed, advance to the next floor
+		if(cur_timestep-last_timestep_on_floor>=spf){
+			
+		}
 	}
 }
