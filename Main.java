d49359bb45cdb0540e3573a4e9d16f67ee8c5baa
David Fountain <David Fountain>
2017-02-23 19:16:01 -0700

    commented on what could be done in Main

--- Main.java
+++ Main.java
@@ -4,4 +4,17 @@ public class Main{
 		Config c = new Config(args[0]);//the first command line argument is the path of the config file
 		System.out.println(c);
 	}
+
+	/*for each timestep:
+		each floor checks for new people
+		the elevator checks to see of it moves onto the next floor
+		if the elevator is at a floor, let off people at that floor and pick up waiting people.
+
+		print to a file, the person instance and the time step they got on the elevator, the floor they got on,
+		the floor they got off, (maybe the total floors they traveled?)
+		*/
+
+		
+
 }
+
